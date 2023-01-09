b36d23d0a954fb5bb58357fc087b7de68c08c6b5
ycomb1nator <hamzamateen1583@gmail.com>
2023-01-09 21:20:57 +0500

    Add forces, directions, and angles and support

--- PyMunk Practice/tut.py
+++ PyMunk Practice/tut.py
@@ -3,36 +3,49 @@ import pymunk
 import math 
 import pymunk.pygame_util 
 
-# pymunk is unitless library, it is upto us to choose the units in the simulations 
+import random
+from typing import Tuple
 
+# pymunk is unitless library, it is upto us to choose the units in the simulations 
 pygame.init()
 
 WIDTH, HEIGHT = 1000, 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-def draw(space, window, draw_options):
-    window.fill("white")
-    space.debug_draw(draw_options)
+def calculate_distance(p1, p2):
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+    
 
+# the angle is in radians
+def calculate_angle(p1, p2):
+    return math.atan2(p2[1] - p1[1], p2[0] - p1[0])
+
+
+def draw(space, window, draw_options, line : Tuple[Tuple[int, int], Tuple[int, int]]):
+    window.fill("white")
+    
+    if line:
+        pygame.draw.line(window, "red", line[0], line[1], 3)
+    space.debug_draw(draw_options)
     pygame.display.update()
     
-def create_ball(space, radius, mass):
-    body = pymunk.Body()    
-    body.position = (WIDTH/2, HEIGHT/2)
+def create_ball(space, radius, mass, pos) -> pymunk.Circle:
+    body = pymunk.Body(body_type=pymunk.Body.STATIC) 
+    body.position = pos
 
     # (0, 0) is top-left corner of the 'window' object
     # attach a circle polygon to this body 
 
-    circle = pymunk.Circle(body, 50) # radius = 50
+    circle = pymunk.Circle(body, radius) # radius = 50
     circle.mass = mass
-    circle.color = (166, 10, 7, 100)
+    circle.color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+
 
     # without these props specified, the ball won't bounce off the surface.
     circle.elasticity = 0.9
     circle.friction = 0.4
     
     space.add(body, circle)
-    
     return circle  
 
 def create_square (space, side, mass):
@@ -67,8 +80,12 @@ def create_boundaries(space, width=WIDTH, height=HEIGHT):
         shape.elasticity = 0.4
         shape.friction = 0.3
 
-        space.add(body, shape)
+        space.add(body, shape) # global space objectS
 
+
+# the idea is to launch the ball towards the mouse upon a click
+# where the power with which the ball gets projectiled towards the mouse 
+# is proportional to the distance between the ball and mouse.
 
 
 # main event loop 
@@ -84,15 +101,19 @@ def run(window, width, height):
 
     draw_options = pymunk.pygame_util.DrawOptions(window)
 
-    bounds = create_boundaries(space)
+    create_boundaries(space)
 
-    ball = create_ball(space, 30, 40)
-    # square = create_square(space, 100, 100)
-    
+    pressed_pos = None 
+    ball = None 
+
     while run: 
         
+        line = None
         # event checking loop 
         for event in pygame.event.get():
+            if ball and pressed_pos:
+                line = [pressed_pos, pygame.mouse.get_pos()]
+
             if event.type == pygame.QUIT: # triggered when red button clicked
                 run = False 
                 break 
@@ -100,9 +121,30 @@ def run(window, width, height):
             # here, if a large impulse if applied, the ball might escape out of the window
             # because the program is checking for the collision event every 1/fps seconds
             if event.type == pygame.MOUSEBUTTONDOWN:
-                ball.body.apply_impulse_at_local_point((-50000, -600), (0, 0))
+                if not ball:
+                    radius = random.randint(15, 75)
 
-        draw(space, window, draw_options)
+                    pressed_pos = pygame.mouse.get_pos()
+                    ball = create_ball(space, radius, 10, pressed_pos)
+
+                elif pressed_pos:
+                    ball.body.body_type = pymunk.Body.DYNAMIC
+
+                    angle = calculate_angle(*line)
+                    force = calculate_distance(*line) * 100
+
+                    x_dir_force = math.cos(angle) * force
+                    y_dir_force = math.sin(angle) * force 
+
+                    ball.body.apply_impulse_at_local_point((x_dir_force, y_dir_force), (0, 0))
+                    pressed_pos = None         
+
+                else:  
+                    # remove ball from screen/window and from body       
+                    space.remove(ball, ball.body)
+                    ball = None
+                    
+        draw(space, window, draw_options, line)
 
         space.step(1/fps) 
         clock.tick(fps)
