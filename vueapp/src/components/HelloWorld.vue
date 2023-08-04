260a3b0d5a0ca812016a0a8392aeb3e68c379ee8
Hamza Mateen <hamzam@neuroleapmail.com>
2023-08-04 12:35:34 +0500

    Add vue counter web app

---
+++ vueapp/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string, otherMessage: string }>()
+
+const count = ref(0)
+
+</script>
+
+<template> 
+  <div class="button">
+    <h1>Count is {{ count }}</h1>
+
+    <button type="button" @click="count++">Increment</button>
+    <button type="button" @click="count <= 0 ? count : count--">Decrement</button>
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
